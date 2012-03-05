3239247e67f4b850679bed336d41ce6521f54e37
Alex Little <alxlit@gmail.com>
2012-03-04 23:03:34 -0500

    updated lessphp to v0.3.3

--- vendor/lessphp/lessc.inc.php
+++ vendor/lessphp/lessc.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * lessphp v0.3.2
+ * lessphp v0.3.3
  * http://leafo.net/lessphp
  *
  * LESS css compiler, adapted from http://lesscss.org
@@ -33,7 +33,7 @@
  *
  */
 class lessc {
-	public static $VERSION = "v0.3.2-dev";
+	public static $VERSION = "v0.3.3";
 	protected $buffer;
 	protected $count;
 	protected $line;
@@ -55,6 +55,10 @@ class lessc {
 	public $imPrefix = '!'; // special character to add !important
 	public $parentSelector = '&';
 
+	// set to the parser that generated the current line when compiling
+	// so we know how to create error messages
+	protected $sourceParser = null;
+
 	static protected $precedence = array(
 		'=<' => 0,
 		'>=' => 0,
@@ -73,6 +77,9 @@ class lessc {
 	// types that have delayed computation
 	static protected $dtypes = array('expression', 'variable',
 		'function', 'negative', 'list', 'lookup');
+
+	// these properties will supress division unless it's inside parenthases
+	static protected $supressDivisionProps = array('/border-radius$/i', '/^font$/i');
 
 	/**
 	 * @link http://www.w3.org/TR/css3-values/
@@ -148,9 +155,9 @@ class lessc {
 		
 		// setting a property
 		if ($this->keyword($key) && $this->assign() &&
-			$this->propertyValue($value) && $this->end())
+			$this->propertyValue($value, $key) && $this->end())
 		{
-			$this->append(array('assign', $key, $value));
+			$this->append(array('assign', $key, $value), $s);
 			return true;
 		} else {
 			$this->seek($s);
@@ -172,7 +179,7 @@ class lessc {
 			if ($this->literal('@charset') && $this->propertyValue($value) &&
 				$this->end())
 			{
-				$this->append(array('charset', $value));
+				$this->append(array('charset', $value), $s);
 				return true;
 			} else {
 				$this->seek($s);
@@ -215,7 +222,7 @@ class lessc {
 		if ($this->variable($var) && $this->assign() &&
 			$this->propertyValue($value) && $this->end())
 		{
-			$this->append(array('assign', $var, $value));
+			$this->append(array('assign', $var, $value), $s);
 			return true;
 		} else {
 			$this->seek($s);
@@ -229,14 +236,14 @@ class lessc {
 				} else {
 					$path = $this->findImport($url);
 					if (!is_null($path)) {
-						$this->append(array('import', $path));
+						$this->append(array('import', $path), $s);
 						return true;
 					}
 				}
 			}
 
 			$this->append(array('raw', '@import url("'.$url.'")'.
-				($media ? ' '.$media : '').';'));
+				($media ? ' '.$media : '').';'), $s);
 			return true;
 		}
 
@@ -268,7 +275,7 @@ class lessc {
 				$block = $this->pop();
 			} catch (exception $e) {
 				$this->seek($s);
-				$this->throwParseError($e->getMessage());
+				$this->throwError($e->getMessage());
 			}
 
 			$hidden = true;
@@ -279,7 +286,7 @@ class lessc {
 				}
 			}
 
-			if (!$hidden) $this->append(array('block', $block));
+			if (!$hidden) $this->append(array('block', $block), $s);
 
 			foreach ($block->tags as $tag) {
 				$this->env->children[$tag][] = $block;
@@ -293,7 +300,7 @@ class lessc {
 			($this->argumentValues($argv) || true) && $this->end())
 		{
 			$tags = $this->fixTags($tags);
-			$this->append(array('mixin', $tags, $argv));
+			$this->append(array('mixin', $tags, $argv), $s);
 			return true;
 		} else {
 			$this->seek($s);
@@ -382,6 +389,15 @@ class lessc {
 
 		// try to find a valid operator
 		while ($this->match(self::$operatorString.($needWhite ? '\s' : ''), $m) && self::$precedence[$m[1]] >= $minP) {
+			if (!$this->inParens && isset($this->env->currentProperty) && $m[1] == "/") {
+				foreach (self::$supressDivisionProps as $pattern) {
+					if (preg_match($pattern, $this->env->currentProperty)) {
+						$this->env->supressedDivision = true;
+						break 2;
+					}
+				}
+			}
+
 			// get rhs
 			$s = $this->seek();
 			$p = $this->inParens;
@@ -421,8 +437,10 @@ class lessc {
 	}
 
 	// consume a list of values for a property
-	function propertyValue(&$value) {
+	function propertyValue(&$value, $keyName=null) {
 		$values = array();	
+
+		if (!is_null($keyName)) $this->env->currentProperty = $keyName;
 		
 		$s = null;
 		while ($this->expressionList($v)) {
@@ -432,6 +450,8 @@ class lessc {
 		}
 
 		if ($s) $this->seek($s);
+
+		if (!is_null($keyName)) unset($this->env->currentProperty);
 
 		if (count($values) == 0) return false;
 
@@ -446,9 +466,20 @@ class lessc {
 
 		// see if there is a negation
 		$s = $this->seek();
-		if ($this->literal('-', false) && $this->variable($var)) {
-			$value = array('negative', array('variable', $var));
-			return true;
+		if ($this->literal('-', false)) {
+			$value = null;
+			if ($this->variable($var)) {
+				$value = array('variable', $var);
+			} elseif ($this->buffer{$this->count} == "(" && $this->expression($exp)) {
+				$value = $exp;
+			} else {
+				$this->seek($s);
+			}
+
+			if (!is_null($value)) {
+				$value = array('negative', $value);
+				return true;
+			}
 		} else {
 			$this->seek($s);
 		}
@@ -503,6 +534,15 @@ class lessc {
 			$this->seek($s);
 		}
 
+		// the spare / when supressing division
+		if (!empty($this->env->supressedDivision)) {
+			unset($this->env->supressedDivision);
+			if ($this->literal("/")) {
+				$value = array('keyword', '/');
+				return true;
+			}
+		}
+
 		return false;
 	}
 
@@ -536,12 +576,26 @@ class lessc {
 	}
 
 	// a list of media types, very lenient
-	function mediaTypes(&$types) {
+	function mediaTypes(&$parts) {
+		$parts = array();
+		while ($this->to("(", $chunk, false, "[^{]")) {
+			$parts[] = array('raw', $chunk."(");
+			$s = $this->seek();
+			if ($this->keyword($name) && $this->assign() &&
+				$this->propertyValue($value))
+			{
+				$parts[] = array('assign', $name, $value);
+			} else {
+				$this->seek($s);
+			}
+		}
+
 		if ($this->to('{', $rest, true, true)) {
-			$types = trim($rest);
+			$parts[] = array('raw', $rest);
 			return true;
 		}
 
+		$parts = null;
 		return false;
 	}
 
@@ -601,24 +655,11 @@ class lessc {
 	 * $allowed restricts the types that are matched.
 	 */
 	function unit(&$unit, $allowed = null) {
-		$simpleCase = $allowed == null;
 		if (!$allowed) $allowed = self::$units;
 
-		if ($this->match('(-?[0-9]*(\.)?[0-9]+)('.implode('|', $allowed).')?', $m, !$simpleCase)) {
+		if ($this->match('(-?[0-9]*(\.)?[0-9]+)('.implode('|', $allowed).')?', $m)) {
 			if (!isset($m[3])) $m[3] = 'number';
 			$unit = array($m[3], $m[1]);
-
-			// check for size/height font unit.. should this even be here?
-			if ($simpleCase) {
-				$s = $this->seek();
-				if (!$this->inExp && $this->literal('/', false) && $this->unit($right, self::$units)) {
-					$unit = array('keyword', $this->compileValue($unit).'/'.$this->compileValue($right));
-				} else {
-					// get rid of whitespace
-					$this->seek($s);
-					$this->match('', $_);
-				}
-			}
 
 			return true;
 		}
@@ -783,7 +824,7 @@ class lessc {
 	function func(&$func) {
 		$s = $this->seek();
 
-		if ($this->match('(%|[\w\-_][\w\-_:\.]*)', $m) && $this->literal('(')) {
+		if ($this->match('(%|[\w\-_][\w\-_:\.]+|[\w_])', $m) && $this->literal('(')) {
 			$fname = $m[1];
 
 			$s_pre_args = $this->seek();
@@ -938,6 +979,37 @@ class lessc {
 		return $target;
 	}
 
+	// import all imports into the block
+	function mixImports($block) {
+		$props = array();
+		foreach ($block->props as $prop) {
+			if ($prop[0] == 'import') {
+				list(, $path) = $prop;
+				$this->addParsedFile($path);
+				$child_less = $this->createChild($path);
+				$root = $child_less->parseTree();
+
+				$root->parent = $block;
+				$this->mixImports($root);
+
+				// inject imported blocks into this block, local will overwrite import
+				$block->children = array_merge($root->children, $block->children);
+
+				// splice in all the props
+				foreach ($root->props as $sub_prop) {
+					if (isset($sub_prop[-1])) {
+						// leave a reference to the imported file for error messages
+						$sub_prop[-1] = array($child_less, $sub_prop[-1]);
+					}
+					$props[] = $sub_prop;
+				}
+			} else {
+				$props[] = $prop;
+			}
+		}
+		$block->props = $props;
+	}
+
 	/**
 	 * Recursively compiles a block. 
 	 * @param $block the block
@@ -974,12 +1046,17 @@ class lessc {
 			$tags = $this->multiplyTags($parent_tags, $block->tags);
 		}
 
-		$this->pushEnv();
+		$env = $this->pushEnv();
+		$env->nameDepth = array();
+
 		$lines = array();
 		$blocks = array();
+		$this->mixImports($block);
 		foreach ($block->props as $prop) {
 			$this->compileProp($prop, $block, $tags, $lines, $blocks);
 		}
+
+		$block->scope = $env;
 
 		$this->pop();
 
@@ -991,7 +1068,7 @@ class lessc {
 		if ($special_block) {
 			$this->indentLevel--;
 			if (isset($block->media)) {
-				echo "@media ".$block->media;
+				echo $this->compileMedia($block);
 			} elseif (isset($block->keyframes)) {
 				echo $block->tags[0]." ".
 					$this->compileValue($this->reduce($block->keyframes));
@@ -1027,7 +1104,6 @@ class lessc {
 
 		return ob_get_clean();
 	}
-
 
 	// find the fully qualified tags for a block and its parent's tags
 	function multiplyTags($parents, $current) {
@@ -1196,11 +1272,25 @@ class lessc {
 
 	// compile a prop and update $lines or $blocks appropriately
 	function compileProp($prop, $block, $tags, &$_lines, &$_blocks) {
+		// set error position context
+		if (isset($prop[-1])) {
+			if (is_array($prop[-1])) {
+				list($less, $count) = $prop[-1];
+				$parentParser = $this->sourceParser;
+				$this->sourceParser = $less;
+				$this->count = $count;
+			} else {
+				$this->count = $prop[-1];
+			}
+		} else {
+			$this->count = -1;
+		}
+
 		switch ($prop[0]) {
 		case 'assign':
 			list(, $name, $value) = $prop;
 			if ($name[0] == $this->vPrefix) {
-				$this->set($name, $this->reduce($value));
+				$this->set($name, $value);
 			} else {
 				$_lines[] = "$name:".
 					$this->compileValue($this->reduce($value)).";";
@@ -1221,6 +1311,12 @@ class lessc {
 			}
 
 			foreach ($mixins as $mixin) {
+				$old_scope = null;
+				if (isset($mixin->parent->scope)) {
+					$old_scope = $this->env;
+					$this->env = $mixin->parent->scope;
+				}
+
 				$have_args = false;
 				if (isset($mixin->args)) {
 					$have_args = true;
@@ -1238,32 +1334,26 @@ class lessc {
 				$mixin->parent = $old_parent;
 
 				if ($have_args) $this->pop();
+
+				if ($old_scope) {
+					$this->env = $old_scope;
+				}
 			}
 
 			break;
 		case 'raw':
 			$_lines[] = $prop[1];
 			break;
-		case 'import':
-			list(, $path) = $prop;
-			$this->addParsedFile($path);
-			$root = $this->createChild($path)->parseTree();
-
-			$root->parent = $block;
-			foreach ($root->props as $sub_prop) {
-				$this->compileProp($sub_prop, $root, $tags, $_lines, $_blocks);
-			}
-
-			// inject imported blocks into this block, local will overwrite import
-			$block->children = array_merge($root->children, $block->children);
-			break;
 		case 'charset':
 			list(, $value) = $prop;
 			$_lines[] = '@charset '.$this->compileValue($this->reduce($value)).';';
 			break;
 		default:
-			echo "unknown op: {$prop[0]}\n";
-			throw new exception();
+			$this->throwError("unknown op: {$prop[0]}\n");
+		}
+
+		if (isset($parentParser)) {
+			$this->sourceParser = $parentParser;
 		}
 	}
 
@@ -1314,10 +1404,15 @@ class lessc {
 			// [2] - green component
 			// [3] - blue component
 			// [4] - optional alpha component
-			if (count($value) == 5) { // rgba
-				return 'rgba('.$value[1].','.$value[2].','.$value[3].','.$value[4].')';
+			list(, $r, $g, $b) = $value;
+			$r = round($r);
+			$g = round($g);
+			$b = round($b);
+
+			if (count($value) == 5 && $value[4] != 1) { // rgba
+				return 'rgba('.$r.','.$g.','.$b.','.$value[4].')';
 			}
-			return sprintf("#%02x%02x%02x", $value[1], $value[2], $value[3]);
+			return sprintf("#%02x%02x%02x", $r, $g, $b);
 		case 'function':
 			// [1] - function name
 			// [2] - some array value representing arguments, either ['string', value] or ['list', ',', values[]]
@@ -1331,6 +1426,21 @@ class lessc {
 		default: // assumed to be unit	
 			return $value[1].$value[0];
 		}
+	}
+
+	function compileMedia($block) {
+		$mediaParts = array();
+		foreach ($block->media as $part) {
+			if ($part[0] == "raw") {
+				$mediaParts[] = $part[1];
+			} elseif ($part[0] == "assign") {
+				list(, $propName, $propVal) = $part;
+				$mediaParts[] = "$propName: ".
+					$this->compileValue($this->reduce($propVal));
+			}
+		}
+
+		return "@media ".trim(implode($mediaParts));
 	}
 
 	function lib_isnumber($value) {
@@ -1363,7 +1473,7 @@ class lessc {
 
 	function lib_rgbahex($color) {
 		if ($color[0] != 'color')
-			throw new exception("color expected for rgbahex");
+			$this->throwError("color expected for rgbahex");
 
 		return sprintf("#%02x%02x%02x%02x",
 			isset($color[4]) ? $color[4]*255 : 0,
@@ -1486,7 +1596,10 @@ class lessc {
 		list($color, $delta) = $this->colorArgs($args);
 
 		$hsl = $this->toHSL($color);
-		$hsl[1] = $this->clamp($hsl[1] + $delta, 360);
+
+		$hsl[1] = $hsl[1] + $delta % 360;
+		if ($hsl[1] < 0) $hsl[1] += 360;
+
 		return $this->toRGB($hsl);
 	}
 
@@ -1542,8 +1655,8 @@ class lessc {
 	// mix(@color1, @color2, @weight);
 	// http://sass-lang.com/docs/yardoc/Sass/Script/Functions.html#mix-instance_method
 	function lib_mix($args) {
-		if ($args[0] != "list")
-			throw new exception("mix expects (color1, color2, weight)");
+		if ($args[0] != "list" || count($args[2]) < 3)
+			$this->throwError("mix expects (color1, color2, weight)");
 
 		list($first, $second, $weight) = $args[2];
 		$first = $this->assertColor($first);
@@ -1574,7 +1687,7 @@ class lessc {
 
 	function assertColor($value, $error = "expected color value") {
 		$color = $this->coerceColor($value);
-		if (is_null($color)) throw new exception($error);
+		if (is_null($color)) $this->throwError($error);
 		return $color;
 	}
 
@@ -1649,7 +1762,8 @@ class lessc {
 			$b = $this->toRGB_helper($H - 1/3, $temp1, $temp2);
 		}
 
-		$out = array('color', round($r*255), round($g*255), round($b*255));
+		// $out = array('color', round($r*255), round($g*255), round($b*255));
+		$out = array('color', $r*255, $g*255, $b*255);
 		if (count($color) > 4) $out[] = $color[4]; // copy alpha
 		return $out;
 	}
@@ -1719,18 +1833,33 @@ class lessc {
 
 	// reduce a delayed type to its final value
 	// dereference variables and solve equations
-	function reduce($var, $defaultValue = array('number', 0)) {
+	function reduce($var) {
+		// this is done here for infinite loop checking
+		if ($var[0] == "variable") {
+			$key = is_array($var[1]) ?
+				$this->vPrefix.$this->toName($this->reduce($var[1])) : $var[1];
+
+			$seen =& $this->env->seenNames;
+
+			if (!empty($seen[$key])) {
+				$this->throwError("infinite loop detected: $key");
+			}
+
+			$seen[$key] = true;
+
+			$out = $this->reduce($this->get($key));
+
+			$seen[$key] = false;
+
+			return $out;
+		}
+
 		while (in_array($var[0], self::$dtypes)) {
 			if ($var[0] == 'list') {
 				foreach ($var[2] as &$value) $value = $this->reduce($value);
 				break;
 			} elseif ($var[0] == 'expression') {
 				$var = $this->evaluate($var[1], $var[2], $var[3]);
-			} elseif ($var[0] == 'variable') {
-				$key = is_array($var[1]) ?
-					$this->vPrefix.$this->toName($this->reduce($var[1])) : $var[1];
-
-				$var = $this->get($key);
 			} elseif ($var[0] == 'lookup') {
 				// do accessor here....
 				$var = array('number', 0);
@@ -1851,7 +1980,6 @@ class lessc {
 		foreach (range(1, 3) as $i) {
 			if ($c[$i] < 0) $c[$i] = 0;
 			if ($c[$i] > 255) $c[$i] = 255;
-			$c[$i] = floor($c[$i]);
 		}
 
 		return $c;
@@ -1890,11 +2018,11 @@ class lessc {
 				$out[] = $lval % $rval;
 				break;
 			case '/':
-				if ($rval == 0) throw new exception("evaluate error: can't divide by zero");
+				if ($rval == 0) $this->throwError("evaluate error: can't divide by zero");
 				$out[] = $lval / $rval;
 				break;
 			default:
-				throw new exception('evaluate error: color op number failed on op '.$op);
+				$this->throwError('evaluate error: color op number failed on op '.$op);
 			}
 		}
 		return $this->fixColor($out);
@@ -1902,11 +2030,8 @@ class lessc {
 
 	// operator on two numbers
 	function op_number_number($op, $left, $right) {
-		if ($right[0] == '%') $right[1] /= 100;
-
-		// figure out type
-		if ($right[0] == 'number' || $right[0] == '%') $type = $left[0];
-		else $type = $right[0];
+		$type = is_null($left) ? "number" : $left[0];
+		if ($type == "number") $type = $right[0];
 
 		$value = 0;
 		switch ($op) {
@@ -1923,7 +2048,7 @@ class lessc {
 			$value = $left[1] % $right[1];
 			break;	
 		case '/':
-			if ($right[1] == 0) throw new exception('parse error: divide by zero');
+			if ($right[1] == 0) $this->throwError('parse error: divide by zero');
 			$value = $left[1] / $right[1];
 			break;
 		case '<':
@@ -1935,7 +2060,7 @@ class lessc {
 		case '=<':
 			return $this->toBool($left[1] <= $right[1]);
 		default:
-			throw new exception('parse error: unknown number operator: '.$op);	
+			$this->throwError('parse error: unknown number operator: '.$op);
 		}
 
 		return array($type, $value);
@@ -1989,7 +2114,8 @@ class lessc {
 	}
 
 	// append an property
-	function append($prop) {
+	function append($prop, $pos = null) {
+		if (!is_null($pos)) $prop[-1] = $pos;
 		$this->env->props[] = $prop;
 	}
 
@@ -2036,8 +2162,13 @@ class lessc {
 
 	// advance counter to next occurrence of $what
 	// $until - don't include $what in advance
+	// $allowNewline, if string, will be used as valid char set
 	function to($what, &$out, $until = false, $allowNewline = false) {
-		$validChars = $allowNewline ? "." : "[^\n]";
+		if (is_string($allowNewline)) {
+			$validChars = $allowNewline;
+		} else {
+			$validChars = $allowNewline ? "." : "[^\n]";
+		}
 		if (!$this->match('('.$validChars.'*?)'.$this->preg_quote($what), $m, !$until)) return false;
 		if ($until) $this->count -= strlen($what); // give back $what
 		$out = $m[1];
@@ -2057,7 +2188,7 @@ class lessc {
 	// match something without consuming it
 	function peek($regex, &$out = null) {
 		$r = '/'.$regex.'/Ais';
-		$result =  preg_match($r, $this->buffer, $out, null, $this->count);
+		$result = preg_match($r, $this->buffer, $out, null, $this->count);
 		
 		return $result;
 	}
@@ -2104,7 +2235,7 @@ class lessc {
 		while (false !== $this->parseChunk());
 
 		if ($this->count != strlen($this->buffer))
-			$this->throwParseError();
+			$this->throwError();
 
 		if (!is_null($this->env->parent))
 			throw new exception('parse error: unclosed block');
@@ -2142,16 +2273,26 @@ class lessc {
 		return $out;
 	}
 
-	function throwParseError($msg = 'parse error') {
-		$line = $this->line + substr_count(substr($this->buffer, 0, $this->count), "\n");
-		if (isset($this->fileName)) {
-			$loc = $this->fileName.' on line '.$line;
-		} else {
-			$loc = "line: ".$line;
+	/**
+	 * Uses the current value of $this->count to show line and line number
+	 */
+	function throwError($msg = 'parse error') {
+		if (!empty($this->sourceParser)) {
+			$this->sourceParser->count = $this->count;
+			return $this->sourceParser->throwError($msg);
+		} elseif ($this->count > 0) {
+			$line = $this->line + substr_count(substr($this->buffer, 0, $this->count), "\n");
+			if (isset($this->fileName)) {
+				$loc = $this->fileName.' on line '.$line;
+			} else {
+				$loc = "line: ".$line;
+			}
+
+			if ($this->peek("(.*?)(\n|$)", $m))
+				throw new exception($msg.': failed at `'.$m[1].'` '.$loc);
 		}
 
-		if ($this->peek("(.*?)(\n|$)", $m))
-			throw new exception($msg.': failed at `'.$m[1].'` '.$loc);
+		throw new exception($msg);
 	}
 
 	/**
